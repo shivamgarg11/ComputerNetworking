e937fa32841250d700da0baea906ab4c79d77095
Shivam Garg <contestshivamgarg@gmail.com>
2019-01-05 17:07:08 +0530

    previous version

---
+++ app/src/main/java/com/shivam/computernetworking/fragments/Home.java
@@ -0,0 +1,34 @@
+package com.shivam.computernetworking.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import com.shivam.computernetworking.R;
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Home extends android.app.Fragment {
+
+
+    public Home() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+}
